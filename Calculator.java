2e6e0d45b106f4de1b5aca1e466123ee5ab0e679
arif96 <32530483+arif96@users.noreply.github.com>
2018-01-08 19:55:40 +0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator{
+		public int tambah(int x, int y){
+		return x + y;
+
+		}
+			public int kurang(int x, int y){
+		return x + y;		
+		}
+	}
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+	public static void main(String[] args){
+		Operator cal = new Operator();
+		cal.setX(10);
+		cal.setY(5);
+			Calculator cla = new Calculator();
+		System.out.println(cla.tambah(cal.getX(), cal.getY());
+		System.out.println(cla.kurang(cal.getX(), cal.getY());
+
+}		
+}
